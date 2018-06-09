f75836d717a460e95b8d059e7669c4b445725673
kramery <jsus365@gmail.com>
2018-06-09 16:59:58 +0200

    Modifico algunas reglas iptables

--- ansible/respaldo/template/iptables.sh
+++ ansible/respaldo/template/iptables.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
- 
+
 ### BEGIN INIT INFO
 # Provides: sudo
 # Required-Start: $local_fs $remote_fs
@@ -48,11 +48,11 @@ iptables -A INPUT -p udp --sport 53 -i eth0 -j ACCEPT
 #DNS para red aula y DMZ
 
 
-iptables -A FORWARD -p tcp --dport 53 -o eth0 -j ACCEPT
-iptables -A FORWARD -p tcp --sport 53 -i eth0 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 53  -j ACCEPT
+iptables -A FORWARD -p tcp --sport 53  -j ACCEPT
 
-iptables -A FORWARD -p udp --dport 53 -o eth0 -j ACCEPT
-iptables -A FORWARD -p udp --sport 53 -i eth0 -j ACCEPT
+iptables -A FORWARD -p udp --dport 53  -j ACCEPT
+iptables -A FORWARD -p udp --sport 53  -j ACCEPT
 
 
 
@@ -60,11 +60,14 @@ iptables -A FORWARD -p udp --sport 53 -i eth0 -j ACCEPT
 ###########HTTP & HTTPS
 
 
-iptables -A OUTPUT -p tcp --dport 80 -o eth0 -j ACCEPT
-iptables -A INPUT -p tcp --sport 80 -i eth0 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -i eth0 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 80 -o eth0 -j ACCEPT
 
-iptables -A OUTPUT -p tcp --dport 443 -o eth0 -j ACCEPT
-iptables -A INPUT -p tcp --sport 443 -i eth0 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -i eth0 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 443 -o eth0 -j  ACCEPT
+
+iptables -A  OUTPUT -p tcp --sport 10000 -o eth0 -j  ACCEPT
+iptables -A INPUT -p tcp --dport 10000 -i eth0 -j ACCEPT
 
 
 #Permitir la entrada y salida a squid
@@ -76,16 +79,16 @@ iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 8080 -j ACCEPT
 
 
-#iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 3128 -j REDIRECT --to-port 8080
+#iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 3128 -j REDIRECT --to-port                                                                                         8080
 
 
 
 
-#Permitir que la red 191.168.11.0/24 tenga salida al exterior por el interfaz eth0
-#iptables -t nat -A POSTROUTING -o eth0 -s 192.168.11.0/24 -j MASQUERADE
+#Permitir que la red 191.168.11.0/24 tenga salida al exterior por el interfaz et                                                                                        h0
+iptables -t nat -A POSTROUTING -o eth0 -s 192.168.11.0/24 -j MASQUERADE
 
 
-#Aunque ya está prohibido por defecto, se prohibe que ninguna otra conexión web sea posible a menos que se use proxy
+#Aunque ya está prohibido por defecto, se prohibe que ninguna otra conexión web                                                                                         sea posible a menos que se use proxy
 #iptables -A FORWARD -s 192.168.11.0/24 -i eth1 -p tcp --dport 80 -j DROP
 #iptables -A FORWARD -s 192.168.11.0/24 -i eth1 -p tcp --dport 443 -j DROP
 
@@ -96,8 +99,8 @@ iptables -A OUTPUT -p tcp --sport 8080 -j ACCEPT
 
 
 #Redireccionar los puertos 80 y 443 para Dansguardian (http y https)
-#iptables -t nat -A PREROUTING -s 192.168.11.0/24 -p tcp --dport 443 -j REDIRECT --to-port 8080
-#iptables -t nat -A PREROUTING -s 192.168.11.0/24 -p tcp --dport 443 -j REDIRECT --to-port 8080
+#iptables -t nat -A PREROUTING -s 192.168.11.0/24 -p tcp --dport 80 -j REDIRECT                                                                                         --to-port 8080
+#iptables -t nat -A PREROUTING -s 192.168.11.0/24 -p tcp --dport 443 -j REDIRECT                                                                                         --to-port 8080
 
 
 
@@ -121,11 +124,11 @@ iptables -A INPUT -p icmp -i eth2 -j ACCEPT
 
 
 
-#################SSH puedo conectarme desde el servidor a todos sitios, pero no conectame a él
+#################SSH
 
 
-iptables -A OUTPUT -p tcp --dport 22 -o eth0 -j ACCEPT
-iptables -A INPUT -p tcp --sport 22 -i eth0 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -i eth0 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -o eth0 -j ACCEPT
 
 
 
@@ -151,15 +154,14 @@ iptables -A INPUT -p tcp --sport 20:21 -j ACCEPT
 
 #RED CLASE
 
-#iptables -A FORWARD -s 192.168.11.0/24 -i eth1 -p tcp --sport 20:21
-#iptables -A FORWARD -s 192.168.11.0/24 -o eth1 -p tcp --dport 20:21
+iptables -A FORWARD  -p tcp --sport 20:21 -j ACCEPT
+iptables -A FORWARD  -p tcp --dport 20:21 -j ACCEPT
 
-iptables -A INPUT -p tcp --sport 20:21 -i eth1 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 20:21 -o eth1 -j ACCEPT
+#iptables -A INPUT -p tcp --sport 20:21 -i eth1 -j ACCEPT
+#iptables -A OUTPUT -p tcp --dport 20:21 -o eth1 -j ACCEPT
 
 
 #RED DMZ
 
 iptables -A OUTPUT -p tcp --dport 20:21 -o eth2 -j ACCEPT
 iptables -A INPUT -p tcp --sport 20:21 -i eth2 -j ACCEPT
-
